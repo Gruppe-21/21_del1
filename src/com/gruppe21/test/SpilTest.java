1f572d36a81292050165548a692ad18e043b0174
Troels <troelstoy@outlook.com>
2020-09-30 22:36:37 +0200

    Test af spil, fejl måske spottet

--- src/com/gruppe21/test/SpilTest.java
+++ src/com/gruppe21/test/SpilTest.java
@@ -7,18 +7,23 @@ public class SpilTest {
 
         Spil testspil = new Spil("Spiller1", "Spiller2");
 
-        //Regel 4
-        testspil.KørRunde(1,6,true);
-        testspil.KørRunde(5,5,true);
-        testspil.KørRunde(4,4,true);
-        testspil.KørRunde(6,6,true);
+
+        testspil.KørRunde(4,4,true); //Regel 4
+        testspil.KørRunde(6,1,true);
+        testspil.KørRunde(5,1,true);
+        testspil.KørRunde(6,5,true);
         //Regel 3
         testspil.KørRunde(1,1,true);
         //Regel 2
-        testspil.KørRunde(6,5,true);
-        testspil.KørRunde(6,3,true);
-        testspil.KørRunde(4,5,true);
-        testspil.KørRunde(6,5,true);
+        testspil.KørRunde(3,1,true);
+        testspil.KørRunde(6,6,true);
+        testspil.KørRunde(4,4,true);
+        testspil.KørRunde(3,3,true);
+        testspil.KørRunde(1,1,true);
+        testspil.KørRunde(1,2,true);
         testspil.KørRunde(5,5,true);
+        testspil.KørRunde(6,5,true);
+        testspil.KørRunde(6,6,true);
+        testspil.KørRunde(6,6,true);
     }
 }
