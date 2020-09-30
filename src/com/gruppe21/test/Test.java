fe3ce46642a8eec97ab06436f971e42774a68dda
Tobias Maneschijn <s205422@student.dtu.dk>
2020-09-30 19:59:27 +0200

    Fix Nullreferenceexception i den nye spil konstruktør..

--- src/com/gruppe21/test/Test.java
+++ src/com/gruppe21/test/Test.java
@@ -1,15 +1,15 @@
 package com.gruppe21.test;
 
 
-import com.gruppe21.spil.Terning;
+import com.gruppe21.spil.*;
 
 // Brug denne klasse til at køre tests
 public class Test {
     private static int iterationer = 1000000 ;
 
     public static void main(String[] args) {
-        FairTerning.FairTerning(iterationer);
-        TestEnsTerninger.TestEnsTerninger(iterationer);
+
+
     }
 
 }
