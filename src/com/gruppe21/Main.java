e9da317c3983c2b7909ea69c973dc98a225ff942
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2020-10-01 15:25:37 +0200

    Spørg spillerne om de vil spille igen

--- src/com/gruppe21/Main.java
+++ src/com/gruppe21/Main.java
@@ -2,14 +2,27 @@ package com.gruppe21;
 
 import com.gruppe21.spil.*;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // instantierer spillet
         Spil spil = new Spil();
 
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
         // Starter spillet
-        spil.Play();
+        while (true){
+            spil.Play();
+            while (true){
+                System.out.println("Spil igen? (Y/N):");
+                String str = scanner.nextLine();
+                if (str.equals("Y")) break;
+                else if (str.equals("N")) return;
+                else System.out.println("Ugyldigt input.");
+            }
+        }
+
     }
 }
 
