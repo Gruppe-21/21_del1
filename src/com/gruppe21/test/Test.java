b44cc4178dd4f98cf2f68bfbba70c4eb7fbd4213
Tobias Maneschijn <s205422@student.dtu.dk>
2020-10-01 12:34:37 +0200

    Man vinder nu ikke hvis man slår to X 1 efter man er over 40 points.

--- src/com/gruppe21/test/Test.java
+++ src/com/gruppe21/test/Test.java
@@ -9,7 +9,30 @@ public class Test {
 
     public static void main(String[] args) {
 
+        Spil testspil = new Spil("Spiller1", "Spiller2");
 
+        System.out.println("Turen går til Spiller "+testspil.getAktuelSpillerNummer());
+
+
+        testspil.KørRunde(3,1,true);
+
+        System.out.println("Turen går til Spiller "+testspil.getAktuelSpillerNummer());
+
+        testspil.KørRunde(6,6,true);
+
+        System.out.println("Turen går til Spiller "+testspil.getAktuelSpillerNummer());
+
+        testspil.KørRunde(4,4,true);
+
+        System.out.println("Turen går til Spiller "+testspil.getAktuelSpillerNummer());
+
+        testspil.KørRunde(1,1,true);
+
+        System.out.println("Turen går til Spiller "+testspil.getAktuelSpillerNummer());
+        testspil.KørRunde(1,2,true);
+        System.out.println("Turen går til Spiller "+testspil.getAktuelSpillerNummer());
+
+        testspil.KørRunde(5,5,true);
     }
 
 }
