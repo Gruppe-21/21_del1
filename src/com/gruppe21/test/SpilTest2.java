8f1897ee7c32a05278faff37d87b57d67a8ce0c7
Troels <troelstoy@outlook.com>
2020-10-01 15:36:43 +0200

    Test 1 og 2 færdig

--- src/com/gruppe21/test/SpilTest2.java
+++ src/com/gruppe21/test/SpilTest2.java
@@ -1,4 +1,34 @@
 package com.gruppe21.test;
 
+import com.gruppe21.spil.Spil;
+
 public class SpilTest2 {
+    public static void main(String[] args) {
+
+        Spil testspil2 = new Spil("Player1", "Player2");
+
+        System.out.println("Turen går til  " + testspil2.getAktuelSpiller().getNavn());
+        testspil2.KørRunde(5,6,true);
+
+        System.out.println("Turen går til  " + testspil2.getAktuelSpiller().getNavn());
+        testspil2.KørRunde(6,6,true);
+
+        System.out.println("Turen går til  " + testspil2.getAktuelSpiller().getNavn());
+        testspil2.KørRunde(6,5,true);
+
+        System.out.println("Turen går til  " + testspil2.getAktuelSpiller().getNavn());
+        testspil2.KørRunde(1,3,true);
+
+        System.out.println("Turen går til  " + testspil2.getAktuelSpiller().getNavn());
+        testspil2.KørRunde(6,6,true);
+
+        System.out.println("Turen går til  " + testspil2.getAktuelSpiller().getNavn());
+        testspil2.KørRunde(3,3,true);
+
+        if (testspil2.KørRunde(4,4,true)){
+            System.out.println("Du har vundet Spiller2");
+
+
+        }
+    }
 }
